185057ca86ae804df25c4d705973c4d36e76c25a
logonrm <logonrm@fiap.com.br>
2017-12-05 22:46:38 -0200

    classe de teste

--- src/br/com/fiap/TestaAplicacao.java
+++ src/br/com/fiap/TestaAplicacao.java
@@ -1,12 +1,5 @@
 package br.com.fiap;
 
-import br.com.fiap.dao.ClienteDAO;
-import br.com.fiap.dao.NotaFiscalDAO;
-import br.com.fiap.dao.ProdutoDAO;
-import br.com.fiap.entity.Cliente;
-import br.com.fiap.entity.NotaFiscal;
-import br.com.fiap.entity.Produto;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
